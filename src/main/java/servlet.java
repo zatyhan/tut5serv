fecc51834cb9101191dfd1f7c4be149153953fad
zatyhan <nureizzaty@gmail.com>
2021-11-18 23:33:59 +0000

    edited doPost

--- src/main/java/servlet.java
+++ src/main/java/servlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 
 @WebServlet(urlPatterns={"/patients"}, loadOnStartup =1)
@@ -19,7 +20,7 @@ public class servlet extends HttpServlet{
                 "</head>\n" +
                 "<body>\n" +
                 "<h1> Nur Eizzaty Hamzaid </h1>\n" +
-                "<p> I am not old and I am smart</p>\n" +
+                "<p> :) </p>\n" +
                 "</body>\n" +
                 "</html>");
         resp.getWriter().write(req.getServletPath());
@@ -27,6 +28,9 @@ public class servlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+//        super.doPost(req, resp);
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+ reqBody);
     }
 }
