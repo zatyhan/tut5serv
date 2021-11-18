479456c568b5c435f1259b92269620eea7887f20
zatyhan <nureizzaty@gmail.com>
2021-11-18 11:05:32 +0000

    added HTML file

--- src/main/java/servlet.java
+++ src/main/java/servlet.java
@@ -5,12 +5,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(urlPatterns={"/patients"}, loadOnStartup =1)
 public class servlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello world");
+        resp.getWriter().write("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>zatyhan</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<h1> Nur Eizzaty Hamzaid </h1>\n" +
+                "<p> I am not old and I am smart</p>\n" +
+                "</body>\n" +
+                "</html>");
         resp.getWriter().write(req.getServletPath());
     }
 
