4667ba52de10654ba854202b61117766215d5a71
zatyhan <nureizzaty@gmail.com>
2021-11-25 02:22:14 +0000

    updated Patient class (added new method)

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -5,28 +5,36 @@ public class Patient implements Serializable {
     String id;
     String phoneNumber;
 
-    public Patient(String n)
+    public Patient(String n, String id, String number)
     {
 //        name= new String(); //difference btween instantiating new and use this // ans: does not need to create a new variable?
         this.name= n;
-    }
-
-    public String getId()
-    {
-        return id;
-    }
-
-    public void setId(String id){
         this.id= id;
-    }
-
-    public String getPhoneNumber()
-    {
-        return  phoneNumber;
-    }
-
-    public void setPhoneNumber(String number)
-    {
         this.phoneNumber= number;
     }
+
+//    public Patient(String n)
+//    {
+////        name= new String(); //difference btween instantiating new and use this // ans: does not need to create a new variable?
+//        this.name= n;
+//    }
+//
+//    public String getId()
+//    {
+//        return id;
+//    }
+//
+//    public void setId(String id){
+//        this.id= id;
+//    }
+//
+//    public String getPhoneNumber()
+//    {
+//        return  phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String number)
+//    {
+//        this.phoneNumber= number;
+//    }
 }
